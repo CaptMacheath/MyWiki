e6294ba681519f6303dc646c5e073b668c6c15f0
Matthew Godwin <12636533+CaptMacheath@users.noreply.github.com>
2020-11-02 01:06:06 -0600

    Started work on main menu (article selection)

--- MyWiki/MainWindow.xaml.cs
+++ MyWiki/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -23,6 +24,45 @@ namespace MyWiki
         public MainWindow()
         {
             InitializeComponent();
+
+            Articles.ItemsSource = GetAllArticles().Select(a => a.Title);
+        }
+
+        private List<Article> GetAllArticles()
+        {
+            List<Article> articles = new List<Article>();
+            string connectionString = "Server=localhost\\SQLEXPRESS;Database=WikiDB;Trusted_Connection=True;";
+
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+
+                SqlCommand command = new SqlCommand
+                {
+                    Connection = connection,
+                    CommandText = "SELECT * FROM [dbo].[Articles];"
+                };
+
+                using SqlDataReader dataReader = command.ExecuteReader();
+
+                while (dataReader.Read())
+                {
+                    Article article = new Article
+                    {
+                        Title = dataReader["title"].ToString(),
+                        Text = dataReader["text"].ToString()
+                    };
+
+                    articles.Add(article);
+                }
+            }
+
+            return articles;
+        }
+
+        private void Articles_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            // ...
         }
     }
 }
