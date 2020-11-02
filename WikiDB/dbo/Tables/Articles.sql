3a00413ff8afa95af9700280187615d0c691ce19
Matthew Godwin <12636533+CaptMacheath@users.noreply.github.com>
2020-11-01 22:45:28 -0600

    Add project files.

---
+++ WikiDB/dbo/Tables/Articles.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [dbo].[Articles] (
+    [id]    UNIQUEIDENTIFIER CONSTRAINT [DF_Articles_id] DEFAULT (newid()) NOT NULL,
+    [title] NVARCHAR (MAX)   NOT NULL,
+    [text]  NVARCHAR (MAX)   NOT NULL,
+    CONSTRAINT [PK_Articles] PRIMARY KEY CLUSTERED ([id] ASC)
+);
+
